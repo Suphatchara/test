9c5ffdd413996a4f9a1914dcc0a52d9d617a23d1
Suphatchara Wo <56674051+Suphatchara@users.noreply.github.com>
2019-10-21 02:35:49 +0700

    class

---
+++ Customer.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+
+namespace CSharpIntermediate
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public List<Order> Order;
+
+        public Customer()
+        {
+            Order = new List<Order>();
+        }
+
+        public Customer(int id)
+            : this()
+        {
+            this.Id = id;
+        }
+        public Customer(int id, string name)
+            : this(id)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
